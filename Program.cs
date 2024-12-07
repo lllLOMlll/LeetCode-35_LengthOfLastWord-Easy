b61f858a621e475bcb1578b8b9e6d8c9a460eda4
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-12-07 17:04:54 -0500

    Working on the solution

--- Program.cs
+++ Program.cs
@@ -1,5 +1,34 @@
-﻿public class Solution {
-    public int LengthOfLastWord(string s) {
-        return 1;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        string sTrim = s.Trim();
+        char characterToFind = ' ';
+        int lastSpace = sTrim.LastIndexOf(characterToFind);
+        string lastWord = sTrim.Substring(characterToFind, sTrim.Length);
+        int lastWordLength =  lastWord.Length;
+
+
+        return lastWordLength;
     }
+
+    static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        string s1 = "Hello World";
+        Console.WriteLine(s.LengthOfLastWord(s1));
+
+        string s2 = "   fly me   to   the moon  ";
+        Console.WriteLine(s.LengthOfLastWord(s2));
+
+        string s3 = "luffy is still joyboy";
+        Console.WriteLine(s.LengthOfLastWord(s3));
+    }
+
+
 }
